9fa4ec69c248ab9e1f9303f00d1ff83a827b7dcc
timur <stukantimur811@gmail.com>
2024-10-27 22:10:41 +0200

    created dependencies file

---
+++ dependencies.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+from database import SessionLocal, AsyncSessionLocal
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+async def get_async_db() -> AsyncSession:
+    db = AsyncSessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
