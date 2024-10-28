d9b30225b806758855dd6784e0bd1341011dc13f
Timur <stukantimur811@gmail.com>
2024-10-28 22:37:18 +0200

    created endpoint for getting and retrieving shops

--- shop/router.py
+++ shop/router.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException
 
 from dependencies import get_db
 from shop.models import Shop
-from shop.schemas import ShopCreateSchema, ShopRetrieveSchema
+from shop.schemas import ShopCreateSchema, ShopRetrieveSchema, ShopDetailSchema
 from auth.middleware import get_user_id
 from shop import crud
 
@@ -23,4 +23,27 @@ def create_shop(request: Request, shop: ShopCreateSchema, db: Session = Depends(
         shop=shop,
         user_id=user_id,
         db=db
+    )
+
+
+@router.get("/shops/", response_model=list[ShopRetrieveSchema])
+def get_all_shops(request: Request, my: bool = False, db: Session = Depends(get_db)):
+    if my:
+        user_id = get_user_id(request)
+        return crud.get_current_user_shop_list(
+            db=db,
+            user_id=user_id
+        )
+    return crud.get_all_shops(
+        db=db
+    )
+
+
+@router.get("/shops/{shop_id}/", response_model=ShopDetailSchema)
+def get_details_of_shop(request: Request, shop_id: int, db: Session = Depends(get_db)):
+    get_user_id(request)
+
+    return crud.get_details_of_shop(
+        db=db,
+        shop_id=shop_id
     )
