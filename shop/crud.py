ff9110a330127942c7168c2415d69bb1db7aafc6
Timur <stukantimur811@gmail.com>
2024-10-28 22:35:26 +0200

    created crud functions for managing getting and retrieving shops

--- shop/crud.py
+++ shop/crud.py
@@ -1,3 +1,5 @@
+from typing import List, Type
+
 from sqlalchemy.orm import Session
 
 from product.models import Product
@@ -31,3 +33,18 @@ def create_shop(shop: ShopCreateSchema, user_id: int, db: Session) -> Shop:
             db.refresh(product_obj)
 
     return shop_obj
+
+
+def get_all_shops(db: Session) -> list[Type[Shop]]:
+    return db.query(Shop).all()
+
+
+def get_current_user_shop_list(db: Session, user_id: int) -> list[Type[Shop]]:
+    return db.query(Shop).filter(Shop.user_id == user_id)
+
+
+def get_details_of_shop(db: Session, shop_id: int):
+    shop_from_db = db.query(Shop).filter(Shop.id == shop_id).first()
+    products_to_shop = db.query(Product).filter(Product.shop_id == shop_from_db.id)
+    shop_from_db.products = products_to_shop
+    return shop_from_db
