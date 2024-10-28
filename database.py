0a58d0133d87bb1f9c0ec69ddf27c9dc1dd0dafe
Timur <stukantimur811@gmail.com>
2024-10-28 22:29:45 +0200

    added async engine

--- database.py
+++ database.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 from settings import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    connect_args={"check_same_thread": False},
+    connect_args={"check_same_thread": False}
 )
-async_engine = create_engine(
+async_engine = create_async_engine(
     settings.ASYNC_DATABASE_URL
 )
 
@@ -18,7 +18,7 @@ AsyncSessionLocal = sessionmaker(
     bind=async_engine,
     class_=AsyncSession,
     autocommit=False,
-    autoflush=False,
+    autoflush=False
 )
 
 Base = declarative_base()
