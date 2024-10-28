62600d26940fc48540ce0ee40c2bce465b6573ef
Timur <stukantimur811@gmail.com>
2024-10-28 22:43:40 +0200

    created endpoint for async creating of many products and processing tasks in the background

--- product/router.py
+++ product/router.py
@@ -1,17 +1,45 @@
-from urllib.parse import unquote
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import BackgroundTasks
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from auth.middleware import get_user_id
-from dependencies import get_db
+from database import AsyncSessionLocal
+from dependencies import get_db, get_async_db
 from product.models import Product
-from product.schemas import ProductCreateSeparatelySchema, ProductRetrieveSchema
+from product.schemas import ProductCreateSeparatelySchema, ProductRetrieveSchema, ProductCreateWithShopSchema, \
+    ProductsListCreateSchema
 from product import crud
 from shop.models import Shop
 
 router = APIRouter(prefix="/products", tags=["products"])
+
+
+def check_unique_of_products(
+        shop_id: int,
+        products: ProductsListCreateSchema | ProductCreateSeparatelySchema,
+        db: Session
+):
+    products_for_db = [product.title for product in db.query(Product).filter(Product.shop_id == shop_id)]
+
+    if isinstance(products, ProductsListCreateSchema):
+        duplicates = []
+        for product in products.products:
+            if product.title in products_for_db:
+                duplicates.append(product.title)
+        if duplicates:
+            raise HTTPException(
+                status_code=400,
+                detail=f"his shop already has products with name(s): {", ".join(duplicates)}"
+            )
+    else:
+        if products.title in products_for_db:
+            raise HTTPException(
+                status_code=400,
+                detail=f"this shop already has products with name: {products.title}"
+            )
 
 
 @router.post("/", response_model=ProductRetrieveSchema)
@@ -24,6 +52,12 @@ def create_product(
 
     shop_from_db = db.query(Shop).filter(Shop.id == product.shop_id).first()
 
+    check_unique_of_products(
+        shop_id=product.shop_id,
+        products=product,
+        db=db
+    )
+
     if not shop_from_db or shop_from_db.user_id != user_id:
         raise HTTPException(status_code=400, detail="invalid shop id")
 
@@ -33,11 +67,11 @@ def create_product(
     )
 
 
-@router.delete("/{shop_id}/{product_id}/")
+@router.delete("/{shop_id}/{product_id}/", response_model=None)
 def delete_product(
         request: Request,
         product_id: int,
-        shop_id: id,
+        shop_id: int,
         db: Session = Depends(get_db)
 ):
     user_id = get_user_id(request)
@@ -51,3 +85,36 @@ def delete_product(
         raise HTTPException(status_code=400, detail="no such product in such shop")
 
     crud.delete_product(product_id=product_id, db=db)
+
+
+@router.post("/add/{shop_id}/", response_model=None)
+async def create_many_products(
+        request: Request,
+        shop_id: int,
+        products: ProductsListCreateSchema,
+        background_tasks: BackgroundTasks,  # Перемістіть цей параметр сюди
+        db: Session = Depends(get_db)  # Залиште цей параметр на останньому місці
+):
+    user_id = get_user_id(request)
+
+    shop_from_db = db.query(Shop).filter(Shop.id == shop_id).first()
+    if not shop_from_db or shop_from_db.user_id != user_id:
+        raise HTTPException(status_code=400, detail="no such shop connected to you")
+
+    check_unique_of_products(
+        shop_id=shop_id,
+        products=products,
+        db=db
+    )
+
+    async with AsyncSessionLocal() as async_db:
+        background_tasks.add_task(
+            crud.create_many_products,
+            products=products.products,
+            shop_id=shop_id,
+            db=async_db
+        )
+
+    return JSONResponse(
+        {"message": "Products are being added successfully!"}
+    )
